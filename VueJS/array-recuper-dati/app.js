1cce0ba034cee7c345922b3616aa930227f72790
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-30 18:42:11 +0200

    Aggiunto nuovo method

--- VueJS/array-recuper-dati/app.js
+++ VueJS/array-recuper-dati/app.js
@@ -39,7 +39,11 @@ const app = new Vue({
                 {
                     name: 'Sushi',
                     isGood: 'yes'
-                }
+                },
+                {
+                    name: 'Acqua',
+                    isGood: 'yes'
+                },
             ],
         },
     },
